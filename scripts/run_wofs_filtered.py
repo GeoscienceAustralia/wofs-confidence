b2c2a029fa939219c552d1498ad2d5df555ebbd9
Ashoka Jayawardena <Ashoka.Jayawardena@ga.gov.au>
2018-07-27 16:41:39 +1000

    tile list get and process, non-pbs run complete

--- scripts/run_wofs_filtered.py
+++ scripts/run_wofs_filtered.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
 import pydash
 import subprocess
-import time
+from confidence import Config
 
 
 class TileProcessor(object):
-    def __init__(self, cfg_yaml):
-        pass
+    def __init__(self, cfg: Config):
+        self.cfg = cfg
 
     def get_tile_list(self):
-        # ToDo
-        return [(17, -39), (17, -38)]
+        list_path = self.cfg.cfg['coverage']['tile_list_path']
+        tiles = []
+        with open(list_path, 'r') as f:
+            for line in f:
+                num = [int(str_num) for str_num in line[:-1].split()]
+                tiles.append((num[0], num[1]))
+        return tiles
 
     def get_tiles_to_process(self):
-        # ToDo
-        return [(17, -38), (17, -39), (17, -40), (16, -39)]
+
+        def file_exists(tile):
+            file_name = self.cfg.cfg['wofs_filtered_summary']['filename'].format(tile[0], tile[1])
+            file_path = Path(self.cfg.cfg['wofs_filtered_summary']['filtered_summary_dir']) / Path(file_name)
+            return file_path.is_file()
+
+        tile_list = self.get_tile_list()
+        # remove existing tiles from the list
+        return [tile for tile in tile_list if not file_exists(tile)]
 
     @staticmethod
     def get_number_of_nodes():
@@ -38,5 +51,6 @@ class TileProcessor(object):
 
 
 if __name__ == '__main__':
-    processor = TileProcessor('stuff')
+    config = Config('/home/547/aj9439/PycharmProjects/wofs-confidence/configs/template_client.yaml')
+    processor = TileProcessor(config)
     processor.run()
